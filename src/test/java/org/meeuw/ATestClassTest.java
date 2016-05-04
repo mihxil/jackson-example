443c9bda288ca3b8a4cd139722dd16a13984fdf5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-05-04 09:07:39 +0200

    Details.

--- src/test/java/org/meeuw/ATestClassTest.java
+++ src/test/java/org/meeuw/ATestClassTest.java
@@ -19,27 +19,27 @@ import static org.junit.Assert.assertEquals;
  */
 public class ATestClassTest {
 
-	@Test
-	public void test() throws JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		AnnotationIntrospector introspector = new AnnotationIntrospectorPair(
-			new JacksonAnnotationIntrospector(),
-			new JaxbAnnotationIntrospector(mapper.getTypeFactory())
-		);
+    @Test
+    public void test() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        AnnotationIntrospector introspector = new AnnotationIntrospectorPair(
+            new JacksonAnnotationIntrospector(),
+            new JaxbAnnotationIntrospector(mapper.getTypeFactory())
+        );
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 
-		mapper.setAnnotationIntrospector(introspector);
+        mapper.setAnnotationIntrospector(introspector);
 
 
         ATestClass instance = new ATestClass();
-		instance.collection = new ArrayList<String>();
-		instance.collection.add("A");
+        instance.collection = new ArrayList<String>();
+        instance.collection.add("A");
 
-		assertEquals("{\"objects\":[{\"attribute\":\"test\",\"value\":\"A\"}]}", mapper.writeValueAsString(instance));
+        assertEquals("{\"objects\":[{\"attribute\":\"test\",\"value\":\"A\"}]}", mapper.writeValueAsString(instance));
 
 
 
-	}
+    }
 
 }
